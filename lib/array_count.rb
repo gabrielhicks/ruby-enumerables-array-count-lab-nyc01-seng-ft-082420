3fdccd097baeda1e963ced39f67acdbfc9e13538
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-08 13:14:25 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,15 +1,6 @@
 def count_strings(array)
-  total = 0
-  
   array.count do |ele|
-    
-    if ele.class == String
-      total += 1
-    end
-    
-  end
-  
-    total
+    ele.class == String
 end
 
 def count_empty_strings(array)
